c19e164b015608198a36f2aefbf3fef5e4cc4629
Yusef <youssefmohmed430@gmail.com>
2026-03-26 23:48:10 +0200

    Push Wallets End Points

---
+++ FIXIT.Presentation/Controllers/WalletController.cs
@@ -0,0 +1,15 @@
+﻿namespace FIXIT.Presentation.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WalletController(IServiceManager serviceManager) : ControllerBase
+{
+    [HttpGet("Id")]
+    [Authorize]
+    public async Task<IActionResult> GetWalletByUserId(string Id)
+    {
+        var result = await serviceManager._walletService.GetWalletByUserId(Id);
+
+        return result.IsSuccess ? Ok(result.Value) : BadRequest(result.Error);
+    }
+}

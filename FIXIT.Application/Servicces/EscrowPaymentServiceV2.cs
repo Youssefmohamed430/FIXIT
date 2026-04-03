4bfd4da4680f770417b92daab51c27ff198ba10b
Yusef <youssefmohmed430@gmail.com>
2026-04-03 15:09:25 +0200

    Tes editts a code and handle errors

--- FIXIT.Application/Servicces/EscrowPaymentServiceV2.cs
+++ FIXIT.Application/Servicces/EscrowPaymentServiceV2.cs
@@ -1,12 +1,25 @@
 ﻿
+using FIXIT.Domain.Entities;
+
 namespace FIXIT.Application.Servicces;
 
-public class EscrowPaymentServiceV2(IUnitOfWork unitOfWork,IServiceManager serviceManager,ILogger<EscrowPaymentService> logger) : IEscrowPaymentServiceV2
+public class EscrowPaymentServiceV2 : IEscrowPaymentServiceV2
 {
     private readonly Dictionary<WorkStatus, IOrderStatusHandler> _handlers;
+    private readonly IUnitOfWork unitOfWork;
+    private readonly IServiceManager serviceManager;
+    private readonly ILogger<EscrowPaymentService> logger;
+    public EscrowPaymentServiceV2(IUnitOfWork _unitOfWork, IServiceManager _serviceManager, ILogger<EscrowPaymentService> _logger,IEnumerable<IOrderStatusHandler> handlers)
+    {
+        logger = _logger;
+        unitOfWork = _unitOfWork;
+        serviceManager = _serviceManager;
+        _handlers = handlers.ToDictionary(h => h.Status);
+    }
     public async Task<Result<OrderDTO>> ChangeWorkOrderStatus(
         int orderId, WorkStatus newStatus)
     {
+        Result<OrderDTO> result;
         var order = await unitOfWork.GetRepository<Order>()
             .FindAsync(o => o.Id == orderId,
                 new[] { "Offer.ServiceProvider.User.Wallet",
@@ -16,11 +29,15 @@ public class EscrowPaymentServiceV2(IUnitOfWork unitOfWork,IServiceManager servi
             return Result<OrderDTO>.Failure(
                 new Error("Order.NotFound", "Order not found."));
 
-        if (!_handlers.TryGetValue(newStatus, out var handler))
-            return Result<OrderDTO>.Failure(
-                new Error("Order.InvalidStatus", "Invalid status."));
-
-        var result = await handler.HandleAsync(order);
+        if (_handlers.TryGetValue(newStatus, out var handler))
+            result = await handler.HandleAsync(order);
+        else
+        {
+            order.WorkStatus = newStatus;
+            await serviceManager.notifService.NotifyCustomerByJobPostId(order.JobPostId, $"The work status of your order has been changed to {newStatus}.");
+            await serviceManager.notifService.NotifyProviderByOfferId(order.OfferId, $"The work status of the order you are working on has been changed to {newStatus}.");
+            result = Result<OrderDTO>.Success(order.Adapt<OrderDTO>());
+        }
 
         await unitOfWork.GetRepository<Order>().UpdateAsync(order);
         await unitOfWork.SaveAsync();
